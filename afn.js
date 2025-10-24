cfec096f0ace73bfe3b6f2447b717531cda29988
americanoguy <usamericandollar@gmail.com>
2025-10-24 20:31:01 +0400

    responsive menu added

--- afn.js
+++ afn.js
@@ -1,5 +1,20 @@
-const age = confirm("Are you 18 or older?")
-if(age){
-}else{
-    window.open("https://www.google.com/");
+// const age = confirm("Are you 18 or older?")
+
+
+// if(age){
+//     const audio = new Audio("audio/car-engine-roaring-376881.mp3");
+//     audio.play()
+// }else{
+//     window.open("https://www.google.com/");
+// }
+
+
+
+function showHiddenMenu() {
+    const links = document.getElementById('links');
+    if(links.classList.contains("show")){
+        links.classList.remove("show")
+    }else{
+        links.classList.add("show")
+    }
 }
